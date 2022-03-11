67808b3800bd839111bac80b54298fb9621d8800
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-03-12 00:38:52 +0530

    ➕ tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./shared/**/*.{js,ts,jsx,tsx}",
+  ],
+  mode: "jit",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
