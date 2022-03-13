ae18ddd20fc4025ad33268b22b5cd8ec6220e3d3
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-03-14 00:14:27 +0530

    🎨 basic structure pages and nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <div className="bg-main-bg  text-white min-h-screen min-w-full">
-        <h1 className="">Circle</h1>
-      </div>
+     HomePage
     </>
   );
 };
---
+++ pages/people.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const People: NextPage = () => {
+  return (
+    <>
+      <div className="bg-main-bg  text-white min-h-screen min-w-full">
+        <h1 className="">People</h1>
+      </div>
+    </>
+  );
+};
+
+export default People;
