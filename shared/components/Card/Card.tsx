b22e63be573214017cc8a4558d32e254939097d0
Tharun0102 <rallapallitharun14@gmail.com>
2022-03-16 11:47:11 +0530

    add gradient border to card

--- shared/components/Card/Card.tsx
+++ shared/components/Card/Card.tsx
@@ -6,8 +6,10 @@ type Props = {
 
 const Card = (props: Props) => {
   return (
-    <div className="m-2 border-[3px] rounded-md w-max px-4 py-2 border-main-border bg-secondary-bg">
-      <>{props.children}</>
+    <div className="m-2 p-[3px] rounded-md w-max h-max bg-gradient-to-br from-[#7362D1] to-[#618EB0]">
+      <div className="flex flex-col justify-between h-full bg-secondary-bg rounded-md px-4 py-2">
+        <>{props.children}</>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
         "main-bg": "#22232D",
         "main-purple": "#7362D1",
         "secondary-bg": "#343347",
-        "main-border": "#628CB1"
       },
     },
   },
