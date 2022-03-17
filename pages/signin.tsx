6e8379aa17986bf68804fecf07f7f6c15d4645e4
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-03-17 13:08:30 +0530

    sign in page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -4,7 +4,7 @@ const SignIn: NextPage = () => {
   return (
     <>
       <div className="bg-main-bg  text-white min-h-screen min-w-full">
-        <h1 className="">People</h1>
+        <h1 className="">Sign In page</h1>
       </div>
     </>
   );
