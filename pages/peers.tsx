286741d892476c666a0b41d91d1cbaf77e6f049c
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2022-03-25 18:26:24 +0530

    Feat : Cards (#19)
    * assests : peerCard
    
    * assests : project card
    
    * Feat: Peer card
    
    * feat ; project card

--- pages/peers.tsx
+++ pages/peers.tsx
@@ -1,16 +1,44 @@
 import { useState } from "react";
 import type { NextPage } from "next";
 import PeerCard from "../components/peerCard";
+import { PeerDetailsType } from "../shared/schemas/peerDetails.schema";
 
 const Peer: NextPage = () => {
-  const [PeerData, setPeerdata] = useState(["a"]);
+  const testProp: PeerDetailsType = {
+    userName: "lsd",
+    about: "Building things for the web",
+    socials: {
+      discord: "lsd#5801",
+      linkedin: "in/lakshya-dhariwal/",
+      twitter: "Lakshya_OnALoop",
+      github: "lakshya-dhariwal",
+    },
+    skills: ["Frontend", "UI/UX"],
+    openTo: ["Hackathons", "Projects"],
+  };
+
+  const testProp2: PeerDetailsType = {
+    userName: "manthan",
+    about: "sadak sa utha ke star bana dunga",
+    socials: {
+      twitter: "Lakshya_OnALoop",
+    },
+    skills: ["Backend", "Fullstack"],
+    openTo: ["Mentoring"],
+  };
+
+  const testArray = [testProp, testProp2];
+  const [PeerData, setPeerdata] = useState(testArray);
+
   return (
     <>
       <div className="bg-main-bg  text-white min-h-screen min-w-full">
-        <h1 className="">Peer</h1>
+        <h1 className="text-center w-full text-xl text-main-gradient my-2 ">
+          Find Your Peers
+        </h1>
         <div className="">
           {PeerData.map((Peer) => {
-            return <PeerCard />;
+            return <PeerCard data={Peer} />;
           })}
         </div>
       </div>
--- shared/components/Card/Card.tsx
+++ shared/components/Card/Card.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Card = (props: Props) => {
   return (
-    <div className="m-2 p-[3px] rounded-md w-max h-max bg-gradient-to-br from-[#7362D1] to-[#618EB0]">
+    <div className="m-2 p-[3px] rounded-md w-max h-max bg-gradient-to-br from-[#7362D1] to-[#618EB0] backdrop-filter backdrop-blur-lg">
       <div className="flex flex-col justify-between h-full bg-secondary-bg rounded-md px-4 py-2">
         <>{props.children}</>
       </div>
