a3928afbeefae00ca6ae6ec88f9becbf0ad2b13e
Tharun0102 <rallapallitharun14@gmail.com>
2022-03-31 21:18:43 +0530

    gradient fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Loading from "../shared/components/Loading/Loading";
 const Home: NextPage = () => {
   return (
     <div className="flex items-center h-[calc(100vh-60px)]">
-      <div className="home-text text-5xl basis-3/6 px-6 py-2 text-center text-transparent bg-clip-text bg-gradient-to-br from-[#B679D2] via-[#8FD89A] to-[#E8735C]">
+      <div className="home-text text-5xl basis-3/6 px-6 py-2 text-center text-transparent bg-clip-text bg-gradient-to-tr from-[#B679D2] via-[#8FD89A] to-[#E8735C]">
         Build your teams for projects and hackathons
       </div>
       <div className="w-full basis-3/6 flex justify-center">
