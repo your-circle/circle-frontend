769fc930499937cb0dc2fc4577c1e8cffa25f126
PRAKASH <maiprakashkumar@gmail.com>
2022-04-01 23:32:56 +0530

    feat: added new project form

--- api/axios.ts
+++ api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:3001/api/";
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:3000/api/";
 
 export default axios.create({
   baseURL,
