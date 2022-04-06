3e3840ae7cb054f81626e037a618a1b87e17db28
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-04-06 14:57:43 +0530

    small fix

--- pages/peers.tsx
+++ pages/peers.tsx
@@ -7,7 +7,6 @@ import EmptyList from "../shared/components/EmptyList";
 import { PeerDetailsType } from "../shared/schemas/peerDetails.schema";
 
 const Peer: NextPage = (props: any) => {
-  const peers = props.peers.data;
   return (
     <>
       <div className="bg-main-bg text-white min-h-[calc(100vh-60px)] min-w-full flex flex-col items-center">
@@ -15,10 +14,10 @@ const Peer: NextPage = (props: any) => {
           Peers
         </h1>
         <div className="grid grid-cols-1 gap-y-5 lg:grid-cols-2 xl:grid-cols-3">
-          {peers.length === 0 ? (
+          {props?.peers?.data?.length === 0 ? (
             <EmptyList message="No users available" />
           ) : (
-            peers.map((peer: PeerDetailsType, index: any) => {
+            props?.peers?.data?.map((peer: PeerDetailsType, index: any) => {
               return <PeerCard data={peer} key={index} />;
             })
           )}
