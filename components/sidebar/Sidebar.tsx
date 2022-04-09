21c2c7ff479c42a5a1c020f355a7ab71ab807637
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-04-09 05:51:38 +0530

    add filters

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import Image from "next/image";
 const Sidebar: React.FC = () => {
   const [sidebarStatus, setSidebarStatus] = useState(false);
-  const [dropdown, setDropdown] = useState(false);
+  const [dropdownSkills, setDropdownSkills] = useState(false);
+  const [dropdownNeeds, setDropdownNeeds] = useState(false);
   return (
     <>
       <div className="fixed left-0 h-full z-20">
@@ -43,34 +44,190 @@ const Sidebar: React.FC = () => {
                   My Projects
                 </button>
               </div>
-              <div className="">
-                <div
-                  className="flex items-center justify-around px-6 p-2 cursor-pointer"
-                  onClick={() => setDropdown(!dropdown)}
-                >
-                  <h2>Filters</h2>
 
-                  {/* <Image
-                    src={`${
-                      dropdown ? "/images/down-icon.svg" : "/images/up-icon.svg"
-                    }`}
-                    layout="fixed"
-                    height={30}
-                    width={30}
-                  /> */}
+              <div className="">
+                <div className="flex items-center justify-around px-6 pt-5 cursor-pointer">
+                  <h2>Filters</h2>
                 </div>
-                <div className="">
-                  <h3>Looking for</h3>
-                  <input
-                    type="checkbox"
-                    name="Developers"
-                    id="Developers"
-                    className="bg-slate-400"
-                  />
-                  <label htmlFor="Developers"></label>
+                <div className="border-b border-gray-200 py-6">
+                  <h3 className="-my-3 flow-root">
+                    <button
+                      type="button"
+                      className="text-white w-full flex items-center justify-between text-sm hover:text-main-purple"
+                      onClick={() => setDropdownSkills(!dropdownSkills)}
+                    >
+                      <span className="font-medium"> Skills </span>
+                      <span className="ml-6 flex items-center ">
+                        {dropdownSkills ? (
+                          <svg
+                            className="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        ) : (
+                          <svg
+                            className="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        )}
+                      </span>
+                    </button>
+                  </h3>
+                  <div className="">
+                    {dropdownSkills ? (
+                      <div className="pt-6" id="filter-section-0">
+                        <div className="space-y-4">
+                          <div className="flex items-center">
+                            <input
+                              id="filter-color-0"
+                              name="skills"
+                              value="white"
+                              type="checkbox"
+                              className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+                            />
+                            <label
+                              htmlFor="filter-color-0"
+                              className="ml-3 text-sm"
+                            >
+                              Frontend
+                            </label>
+                          </div>
+                          <div className="flex items-center">
+                            <input
+                              id="filter-color-0"
+                              name="skills"
+                              value="white"
+                              type="checkbox"
+                              className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+                            />
+                            <label
+                              htmlFor="filter-color-0"
+                              className="ml-3 text-sm"
+                            >
+                              Backend
+                            </label>
+                          </div>
+                          <div className="flex items-center">
+                            <input
+                              id="filter-color-0"
+                              name="skills"
+                              value="white"
+                              type="checkbox"
+                              className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+                            />
+                            <label
+                              htmlFor="filter-color-0"
+                              className="ml-3 text-sm"
+                            >
+                              Blockchain
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
                 </div>
-                <h3></h3>
-                <h3></h3>
+                <div className="border-b border-gray-200 py-6">
+                  <h3 className="-my-3 flow-root">
+                    <button
+                      type="button"
+                      className="text-white w-full flex items-center justify-between text-sm hover:text-main-purple"
+                      onClick={() => setDropdownNeeds(!dropdownNeeds)}
+                    >
+                      <span className="font-medium"> Looking For </span>
+                      <span className="ml-6 flex items-center ">
+                        {dropdownNeeds ? (
+                          <svg
+                            className="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        ) : (
+                          <svg
+                            className="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        )}
+                      </span>
+                    </button>
+                  </h3>
+                  <div className="">
+                    {dropdownNeeds ? (
+                      <div className="pt-6" id="filter-section-0">
+                        <div className="space-y-4">
+                          <div className="flex items-center">
+                            <input
+                              id="filter-color-0"
+                              name="skills"
+                              value="white"
+                              type="checkbox"
+                              className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+                            />
+                            <label
+                              htmlFor="filter-color-0"
+                              className="ml-3 text-sm"
+                            >
+                              Developers
+                            </label>
+                          </div>
+                          <div className="flex items-center">
+                            <input
+                              id="filter-color-0"
+                              name="skills"
+                              value="white"
+                              type="checkbox"
+                              className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+                            />
+                            <label
+                              htmlFor="filter-color-0"
+                              className="ml-3 text-sm"
+                            >
+                              Mentors
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
