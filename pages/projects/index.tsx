e7288d4dedf0333c5455beb8779b7775c967167b
Tharun0102 <rallapallitharun14@gmail.com>
2022-04-15 12:50:50 +0530

    filters-init

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,45 +3,60 @@ import { useEffect, useState } from "react";
 import ProjectCard from "../../components/projectCard";
 import Sidebar from "../../components/sidebar";
 import EmptyList from "../../shared/components/EmptyList";
+import Loading from "../../shared/components/Loading";
 import { ProjectDetailsType } from "../../shared/schemas/projectDetails.schema";
 import { getAllProjects } from "../../shared/services/projects.services";
 
 const Projects: NextPage = () => {
-  const [range, setRange] = useState({ from: 1, to: 6 });
+  const [filters, setFilters] = useState({
+    from: 1,
+    to: 6,
+    tech: [],
+    need: [],
+  });
   const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchProjects();
-  }, [range]);
+  }, [filters]);
 
   const fetchProjects = async () => {
     try {
-      const res = await getAllProjects(range);
+      setLoading(true);
+      const payload = { ...filters };
+      const res = await getAllProjects(payload);
       setProjects(res.data);
+      setLoading(false);
     } catch (err) {
+      setLoading(false);
       console.error(err);
     }
   };
 
   return (
     <>
-      <Sidebar />
+      <Sidebar type="PROJECTS" filters={filters} setFilters={setFilters} />
       <div className="bg-main-bg text-white min-h-[calc(100vh-60px)] min-w-full flex flex-col items-center">
         <h1 className="text-center w-full text-xl text-main-gradient my-2 ">
           Projects
         </h1>
-        <div className="grid grid-cols-1 gap-y-5 lg:grid-cols-2 xl:grid-cols-3 mb-6">
-          {projects.length === 0 ? (
-            <>
-              <span></span>
-              <EmptyList message="No Projects available" />
-            </>
-          ) : (
-            projects.map((project: ProjectDetailsType, index: number) => {
-              return <ProjectCard data={project} key={index} />;
-            })
-          )}
-        </div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="grid grid-cols-1 gap-y-5 lg:grid-cols-2 xl:grid-cols-3 mb-6">
+            {projects.length === 0 ? (
+              <>
+                <span></span>
+                <EmptyList message="No Projects available" />
+              </>
+            ) : (
+              projects.map((project: ProjectDetailsType, index: number) => {
+                return <ProjectCard data={project} key={index} />;
+              })
+            )}
+          </div>
+        )}
       </div>
     </>
   );
