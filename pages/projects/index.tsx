06ec8701bae8bf16dc57ccffd3361b3fc69139f2
PRAKASH <maiprakashkumar@gmail.com>
2022-04-16 18:34:40 +0530

    feat: added infinity scroll in Project Page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,4 +1,4 @@
-import type { GetStaticProps, NextPage } from "next";
+import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import ProjectCard from "../../components/projectCard";
 import Sidebar from "../../components/sidebar";
@@ -7,21 +7,22 @@ import Loading from "../../shared/components/Loading";
 import { ProjectDetailsType } from "../../shared/schemas/projectDetails.schema";
 import { getAllProjects } from "../../shared/services/projects.services";
 import { BsSearch } from "react-icons/bs";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { toast } from "react-toastify";
 
 const Projects: NextPage = () => {
   const [filters, setFilters] = useState({
-    from: 1,
-    to: 6,
     title: "",
     tech: [],
     need: [],
   });
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    fetchProjects();
-  }, [filters]);
+  const [range, setRange] = useState({
+    from: 1,
+    to: 9,
+  });
+  const [projects, setProjects] = useState<Array<ProjectDetailsType>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [hasMoreProject, setHasMoreProject] = useState<boolean>(true);
 
   const onTitleChange = (e: any) => {
     setFilters({
@@ -29,22 +30,42 @@ const Projects: NextPage = () => {
       title: e.target.value,
     });
   };
+
   const fetchProjects = async () => {
     try {
       setLoading(true);
-      const payload = { ...filters };
+      const payload = { ...filters, ...range };
       const res = await getAllProjects(payload);
-      setProjects(res.data);
-      setLoading(false);
-    } catch (err) {
+      if (res.data.length < 9) {
+        setHasMoreProject(false);
+        setProjects([...projects, ...res.data]);
+      } else {
+        console.log("new data", res.data);
+        setProjects([...projects, ...res.data]);
+        setRange((range) => ({ from: range.from + 9, to: range.to + 9 }));
+        setLoading(false);
+      }
+    } catch (err: any) {
       setLoading(false);
       console.error(err);
+      toast.error(err.message);
     }
   };
 
+  useEffect(() => {
+    fetchProjects();
+  }, [filters]);
+
   return (
     <>
-      <Sidebar type="PROJECTS" filters={filters} setFilters={setFilters} />
+      <Sidebar
+        type="PROJECTS"
+        filters={filters}
+        setFilters={setFilters}
+        setRange={setRange}
+        setProjects={setProjects}
+        setHasMoreProject={setHasMoreProject}
+      />
       <div className="bg-main-bg text-white min-h-[calc(100vh-60px)] min-w-full flex flex-col items-center">
         <h1 className="text-center w-full text-xl text-main-gradient my-2 ">
           Projects
@@ -61,11 +82,20 @@ const Projects: NextPage = () => {
             className="bg-main-gray pl-10 pr-2 pt-2 pb-3 text-l rounded-sm border-[1px] border-gray-600"
           />
         </div>
-        {loading ? (
-          <Loading />
-        ) : (
+
+        <InfiniteScroll
+          dataLength={projects.length}
+          next={fetchProjects}
+          hasMore={hasMoreProject}
+          loader={
+            <div className="mx-auto w-fit">
+              <Loading />
+            </div>
+          }
+          endMessage={<h4 className="text-center mb-4">You Have Seen All</h4>}
+        >
           <div className="grid grid-cols-1 gap-y-5 lg:grid-cols-2 xl:grid-cols-3 mb-6">
-            {projects.length === 0 ? (
+            {projects.length === 0 && !loading ? (
               <>
                 <span></span>
                 <EmptyList message="No Projects available" />
@@ -76,7 +106,7 @@ const Projects: NextPage = () => {
               })
             )}
           </div>
-        )}
+        </InfiniteScroll>
       </div>
     </>
   );
