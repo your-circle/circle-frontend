c0926a9390dd2faa2fba85ae737e4ef00fa7e35b
Tharun0102 <rallapallitharun14@gmail.com>
2022-05-25 17:36:34 +0530

    add empty list icon for light theme

--- shared/components/EmptyList/EmptyList.tsx
+++ shared/components/EmptyList/EmptyList.tsx
@@ -11,13 +11,22 @@ const EmptyList = (props: Props) => {
   return (
     <div className="flex flex-col gap-4 items-center my-10">
       <Image
-        src="/images/open-box.svg"
+        src={
+          theme === "light"
+            ? "/images/open-box-light.svg"
+            : "/images/open-box.svg"
+        }
         alt="empty list"
         height={100}
         width={100}
-        className="text-black"
       />
-      <div className="w-full text-center">{props.message || "Empty List"}</div>
+      <div
+        className={`w-full text-center ${
+          theme === "light" ? "text-black" : "text-white"
+        } text-black`}
+      >
+        {props.message || "Empty List"}
+      </div>
     </div>
   );
 };
